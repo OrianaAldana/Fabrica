5b1faa23fac592e70d0f90c858e99fc6324a2c76
Oriana Aldana <valpoca24@gmail.com>
2025-11-24 20:31:48 -0400

    lisffto

---
+++ Migrations/20251125003139_m1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FabricaNube.Migrations
+{
+    /// <inheritdoc />
+    public partial class m1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ControlesCalidad",
+                columns: table => new
+                {
+                    IdControl = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IdLote = table.Column<int>(type: "integer", nullable: false),
+                    FechaControl = table.Column<DateOnly>(type: "date", nullable: false),
+                    Resultado = table.Column<string>(type: "text", nullable: false),
+                    Responsable = table.Column<string>(type: "text", nullable: true),
+                    DetalleJson = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ControlesCalidad", x => x.IdControl);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LotesProduccion",
+                columns: table => new
+                {
+                    IdLote = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IdOrden = table.Column<int>(type: "integer", nullable: false),
+                    CantidadProducida = table.Column<int>(type: "integer", nullable: false),
+                    FechaProduccion = table.Column<DateOnly>(type: "date", nullable: false),
+                    Estado = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LotesProduccion", x => x.IdLote);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrdenesProduccion",
+                columns: table => new
+                {
+                    IdOrden = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IdProducto = table.Column<int>(type: "integer", nullable: false),
+                    Cantidad = table.Column<int>(type: "integer", nullable: false),
+                    FechaInicio = table.Column<DateOnly>(type: "date", nullable: false),
+                    FechaFin = table.Column<DateOnly>(type: "date", nullable: true),
+                    Estado = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrdenesProduccion", x => x.IdOrden);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    IdProducto = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Codigo = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Nombre = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Categoria = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CostoProduccion = table.Column<decimal>(type: "numeric", nullable: false),
+                    StockActual = table.Column<int>(type: "integer", nullable: false),
+                    StockMinimo = table.Column<int>(type: "integer", nullable: false),
+                    ImagenUrl = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.IdProducto);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SolicitudesDemanda",
+                columns: table => new
+                {
+                    IdSolicitud = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CodSucursal = table.Column<int>(type: "integer", nullable: false),
+                    CodArticulo = table.Column<int>(type: "integer", nullable: false),
+                    Cantidad = table.Column<int>(type: "integer", nullable: false),
+                    FechaSolicitud = table.Column<DateOnly>(type: "date", nullable: false),
+                    Estado = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SolicitudesDemanda", x => x.IdSolicitud);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ControlesCalidad");
+
+            migrationBuilder.DropTable(
+                name: "LotesProduccion");
+
+            migrationBuilder.DropTable(
+                name: "OrdenesProduccion");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "SolicitudesDemanda");
+        }
+    }
+}
